1db5f3082d0c0990b3c3dc72a265210d1c79c5f2
yuya-codecommit-user <yuyakawai.github@gmail.com>
2025-02-11 19:12:59 +0900

    Remove unused image entries and add image_1.png asset

--- images.js
+++ images.js
@@ -5,40 +5,4 @@ export const images = [
     element: null,
     isLoaded: false,
   },
-  {
-    name: "image_2",
-    path: "image/image_2.png",
-    element: null,
-    isLoaded: false,
-  },
-  {
-    name: "image_3",
-    path: "image/image_3.png",
-    element: null,
-    isLoaded: false,
-  },
-  {
-    name: "image_4",
-    path: "image/image_4.png",
-    element: null,
-    isLoaded: false,
-  },
-  {
-    name: "image_5",
-    path: "image/image_5.png",
-    element: null,
-    isLoaded: false,
-  },
-  {
-    name: "image_6",
-    path: "image/image_6.png",
-    element: null,
-    isLoaded: false,
-  },
-  {
-    name: "image_rate",
-    path: "image/image_rate.png",
-    element: null,
-    isLoaded: false,
-  },
 ];
